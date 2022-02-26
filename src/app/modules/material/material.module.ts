9be040ab8587ea3b85a23c9a6eab4f782c3be42b
Camilo Pinzon <capinzon98@gmail.com>
2022-02-26 09:58:38 -0500

    Modulo Devices 26_02_2022

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -4,18 +4,19 @@ import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
-import { MatExpansionModule } from '@angular/material/expansion';
 
 /* Utilities */
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 /* Forms */
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
 
 /* Tables */
 import { MatTableModule } from '@angular/material/table';
@@ -27,14 +28,15 @@ import { MatPaginatorModule } from '@angular/material/paginator';
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
-    MatExpansionModule,
     MatButtonModule,
     MatIconModule,
     MatTabsModule,
     MatCardModule,
+    MatExpansionModule,
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
+    MatRadioModule,
     MatTableModule,
     MatPaginatorModule
   ]
